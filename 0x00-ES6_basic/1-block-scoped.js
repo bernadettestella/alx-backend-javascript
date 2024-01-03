aeac91dc7907cc82c7d2c4c7deefd3251192a784
bernadettestella <bernadettestella27@gmail.com>
2024-01-03 15:22:22 +0300

    JavaScript ES6

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+  var task = false;
+  var task2 = true;
+
+  if (trueOrFalse) {
+    let task = true; //eslint-disable-line no-unused-vars
+    let task2 = false; //eslint-disable-line no-unused-vars
+  }
+
+  return [task, task2];
+}
